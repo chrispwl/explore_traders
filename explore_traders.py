f8e5f80e287eb7834ab9075e0c9dbb7b998af507
raminty <rtmailings@sent.com>
2017-10-04 16:52:18 +0100

    Persist graphml file
    Before getting rid of neo4j, needed to make a global variable to persist in-memory graph between http requests

--- explore_traders.py
+++ explore_traders.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python
 from json import dumps
-from flask import Flask, g, Response, request
+from flask import Flask, g, Response, request, current_app
+# from werkzeug.local import LocalProxy
 import sys, csv, utils
 import networkx as nx
 from neo4j.v1 import GraphDatabase, basic_auth
 
 app = Flask(__name__, static_url_path='/static/')
-# basic auth:
+print("Loading app", app.name)
+print('Loading graph to memory...')
+NETX_DB = nx.Graph()
+NETX_DB = nx.read_gml('impex_full.graphml')
+print('loaded', NETX_DB.order(), 'nodes and', NETX_DB.size(), 'edges')
+
+# basic auth for neo4j db:
 uri = "bolt://127.0.0.1:7687"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "TIComphoeLON"))
-
-def get_db():
+def get_n4jdb():
     if not hasattr(g, 'neo4j_db'):
         g.neo4j_db = driver.session()
     return g.neo4j_db
 
 @app.teardown_appcontext
 def close_db(error):
+    print('App teardown', app.name)
     if hasattr(g, 'neo4j_db'):
         g.neo4j_db.close()
-    # if hasattr(g, 'netx_db'):
-    #     del g.netx_db
 
 def _get_top_nodes(Gph, company, howmany=2):
     """Generate top-most commodities: returns sorted list of (u, v, d)"""
-    all_edges = Gph.edges_iter(nbunch=[company], data='monthcount')
+    all_edges = Gph.edges(nbunch=[company], data='monthcount')
     sorted_edges = sorted(all_edges, key=lambda tup: -int(tup[howmany]))
     return sorted_edges
 
 @app.route("/")
 def get_index():
-    if not hasattr(g, 'netx_db'):
-        print('Loading graph to memory...')
-        g.netx_db = nx.read_gml('impex_full.graphml')
-        print('loaded', g.netx_db.order(), 'nodes and', g.netx_db.size(), 'edges')
     return app.send_static_file('index.html')
 
 def serialize_company(company):
@@ -52,57 +53,55 @@ def serialize_cmdty(cast):
 
 @app.route("/graph")
 def get_graph():
-    db = get_db()
-    results = db.run("MATCH (c:Comcode)<-[:EXPORTED]-(n:Company) "
-             "RETURN n.name as movie, collect(c.comcode) as cast "
-             "LIMIT {limit}", {"limit": request.args.get("limit", 100)})
-    # print('searching for comcodes traded by', rg['name'])
-    # tops = [t for t in _get_top_nodes(Gph, rg['name'])]
-    # if len(tops) > 1:
-    #     top1, top2 = tops[:2]
-    # else:
-    #     raise NotImplementedError
-    # common_HS = [top1[1], top2[1]]
-    # names = [n for n in nx.common_neighbors(Gph, common_HS[0], common_HS[1])]
-    # retdict = {}
-    # for name in names:
-    #     cmdties = dict([(c[1], c[2]) for c in _get_top_nodes(Gph, name)])
-    #     retdict[name] = cmdties
+    global NETX_DB
+    Gph = NETX_DB
     nodes = []
     rels = []
+    tops = [t for t in _get_top_nodes(Gph, 'GODIVA UK LIMITED')]
+    # print(tops)
+    if len(tops) > 1:
+        top1, top2 = tops[:2]
+    else:
+        raise NotImplementedError
+    common_HS = [top1[1], top2[1]]
+    names = [n for n in nx.common_neighbors(Gph, common_HS[0], common_HS[1])]
+    # retdict = {}
     i = 0
-    for j, record in enumerate(results):
-        if j < 100:
-            nodes.append({"title": record["movie"], "label": "movie"})
+    for name in names:
+        if i < 20:
+            nodes.append({"title": name, "label": "Company"})
             target = i
             i += 1
-            for name in record['cast']:
-                actor = {"title": name, "label": "actor"}
+            cmdties = dict([(c[1], c[2]) for c in _get_top_nodes(Gph, name)])
+            for cmdty in cmdties:
+                hsnode = {"title": cmdty, "label": "commodity"}
                 try:
-                    source = nodes.index(actor)
+                    source = nodes.index(hsnode)
                 except ValueError:
-                    nodes.append(actor)
+                    nodes.append(hsnode)
                     source = i
                     i += 1
                 rels.append({"source": source, "target": target})
+    print('sending json for background graph...')
     return Response(dumps({"nodes": nodes, "links": rels}),
-                    mimetype="application/json")
+        mimetype="application/json")
 
 
 @app.route("/search")
 def get_search():
+    # print('in get_search g has', dir(g))
     try:
         q = request.args['q']
     except KeyError:
         return []
     else:
-        db = get_db()
-        results = db.run("MATCH (n:Company) "
+        n4db = get_n4jdb()
+        results = n4db.run("MATCH (n:Company) "
                  "WHERE n.name =~ {coname} "
                  "RETURN n", {"coname": "(?i).*" + q + ".*"}
         )
-        # print('searching for comcodes traded by', rg['name'])
-        # tops = [t for t in _get_top_nodes(Gph, rg['name'])]
+        # print('searching for comcodes traded by', q)
+        # tops = [t for t in _get_top_nodes(db, 'GODIVA%20LIMITED')]
         # if len(tops) > 1:
         #     top1, top2 = tops[:2]
         # else:
@@ -111,10 +110,10 @@ def get_search():
         #     raise NotImplementedError
         # common_HS = [top1[1], top2[1]]
         # print('Identified', common_HS[0], common_HS[1], 'as two top comcodes')
-        # names = [n for n in nx.common_neighbors(Gph, common_HS[0], common_HS[1])]
+        # names = [n for n in nx.common_neighbors(db, common_HS[0], common_HS[1])]
         # retdict = {}
         # for name in names:
-        #     cmdties = dict([(c[1], c[2]) for c in _get_top_nodes(Gph, name)])
+        #     cmdties = dict([(c[1], c[2]) for c in _get_top_nodes(db, name)])
         #     retdict[name] = cmdties
         # return Response(dumps({'competitors': dict(retdict)}),
         #     mimetype="application/json")
@@ -124,22 +123,28 @@ def get_search():
 
 @app.route("/movie/<name>")
 def get_movie(name):
-    db = get_db()
-    results = db.run("MATCH (n:Company {name:{name}}) "
+    n4db = get_n4jdb()
+    results = n4db.run("MATCH (n:Company {name:{name}}) "
              "OPTIONAL MATCH (n)-[r]->(c:Comcode) "
              "RETURN n.name as name,"
              "collect([c.comcode, "
              "         lower(type(r)), r.monthcount]) as cmdties "
              "LIMIT 1", {"name": name})
     result = results.single();
+    global NETX_DB
+    Gph = NETX_DB
+    co_name = Gph.nodes(name)
+
     return Response(dumps({"name": result['name'],
                            "cast": [serialize_cmdty(member)
                                     for member in result['cmdties']]}),
                     mimetype="application/json")
 
+# print('before name=main, g has', dir(g))
 
 if __name__ == '__main__':
     host='127.0.0.1'
     port=8080
     print('Server running on http://{0}:{1}...'.format(host, port))
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)  # , debug=True, use_reloader=False
+
